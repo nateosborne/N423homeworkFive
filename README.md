8af697912dd10ac63fd5ab5f39fabeaa5545ab07
nateosborne <natedresdeno@gmail.com>
2024-11-18 16:10:58 -0500

    readme edit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-https://nateosborne.github.io/N423homeworkFive/
+This is the link to the working website:
+https://nateosborne.github.io/N423homeworkFive/dist/
+
+This application will allow the user to view a list of albums along with selecting a genre to sort the display by.
--- src/model.js
+++ src/model.js
@@ -15,21 +15,32 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export async function showAllAlbums(genres) {
+  $(".showAlbums").empty();
   try {
     const querySnapshot = await getDocs(collection(db, "albums"));
     let albumstring = "";
     querySnapshot.forEach((doc) => {
-      //   if (doc.data().genre == genres) {
-      albumstring += `<div class="album">`;
-      albumstring += `<h3>${doc.data().title}</h3>`;
-      albumstring += `<div class="albumImg"><img src="${
-        doc.data().imageURL
-      }"/></div>`;
-      albumstring += `<p>${doc.data().artist}</p>`;
-      albumstring += `<p>${doc.data().genre}</p>`;
-      albumstring += `</div>`;
-      //   console.log(`${doc.id} => ${doc.data()}`);
-      //   }
+      console.log("genre " + genres);
+      if (doc.data().genre == genres) {
+        albumstring += `<div class="album">`;
+        albumstring += `<h3>${doc.data().title}</h3>`;
+        albumstring += `<div class="albumImg"><img src="${
+          doc.data().imageURL
+        }"/></div>`;
+        albumstring += `<p>${doc.data().artist}</p>`;
+        albumstring += `<p>${doc.data().genre}</p>`;
+        albumstring += `</div>`;
+        //   console.log(`${doc.id} => ${doc.data()}`);
+      } else {
+        albumstring += `<div class="album">`;
+        albumstring += `<h3>${doc.data().title}</h3>`;
+        albumstring += `<div class="albumImg"><img src="${
+          doc.data().imageURL
+        }"/></div>`;
+        albumstring += `<p>${doc.data().artist}</p>`;
+        albumstring += `<p>${doc.data().genre}</p>`;
+        albumstring += `</div>`;
+      }
     });
     $(".showAlbums").append(albumstring);
   } catch (e) {
